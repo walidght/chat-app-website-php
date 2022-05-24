444f4acbe46bccf2d053d3d6c5557b9988765540
Youx <95051755+Youxise@users.noreply.github.com>
2022-05-24 18:29:19 +0100

    ghoze

--- php/search.php
+++ php/search.php
@@ -11,7 +11,7 @@
     if(mysqli_num_rows($query) > 0){
         include_once "data.php";
     }else{
-        $output .= 'No user found related to your search term';
+        $output .= 'Aucun utilisateur trouvé';
     }
     echo $output;
 ?>
