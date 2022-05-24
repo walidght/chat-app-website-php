431268e7c2b09842c7c76da14abcd1edc39af092
Youx <95051755+Youxise@users.noreply.github.com>
2022-05-24 17:12:26 +0100

    Update users.php

--- users.php
+++ users.php
@@ -26,8 +26,8 @@
         <a href="php/logout.php?logout_id=<?php echo $row['unique_id']; ?>" class="logout">Logout</a>
       </header>
       <div class="search">
-        <span class="text">Select an user to start chat</span>
-        <input type="text" placeholder="Enter name to search...">
+        <span class="text">Sélectionner un utilisateur pour commencer à tchatter !</span>
+        <input type="text" placeholder="Rechercher un nom...">
         <button><i class="fas fa-search"></i></button>
       </div>
       <div class="users-list">
