e41028c1337f7ff6685e33f6b73baf03748a83f5
Youx <95051755+Youxise@users.noreply.github.com>
2022-05-24 17:45:59 +0100

    ghoze

--- users.php
+++ users.php
@@ -23,7 +23,7 @@
             <p><?php echo $row['status']; ?></p>
           </div>
         </div>
-        <a href="php/logout.php?logout_id=<?php echo $row['unique_id']; ?>" class="logout">Logout</a>
+        <a href="php/logout.php?logout_id=<?php echo $row['unique_id']; ?>" class="logout">Se déconnecter</a>
       </header>
       <div class="search">
         <span class="text">Sélectionner un utilisateur pour commencer à tchatter !</span>
